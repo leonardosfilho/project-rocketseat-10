92dec2c9f5691ace56e0bcfb8ee7df77fd8d4055
leonardosfilho <leonardosfilho21@gmail.com>
2023-12-20 00:27:27 -0300

    alteração de sound

--- src/sounds.js
+++ src/sounds.js
@@ -2,6 +2,6 @@ export const buttonPressAudio = new Audio('./Assets/la-ele.mp3')
 
 export const kitchenTimer = new Audio('./Assets/beligol-beligol.mp3')
 
-export const bgAudio = new Audio('./Assets/como-e-amigo.mp3')
+export const bgAudio = new Audio('./Assets/neymar-errei-fui-moleque.mp3')
 
 bgAudio.loop = true
